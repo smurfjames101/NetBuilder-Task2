fb3b1b62c9c32b932562903f089adf6d18623949
smurfjames101 <smurfjames@gmail.com>
2019-03-30 22:13:46 +0000

    Create Constants.java

---
+++ src/main/java/com/netbuilder/main/Constants.java
@@ -0,0 +1,11 @@
+package com.netbuilder.main;
+
+import java.util.ArrayList;
+
+import com.netbuilder.entities.Person;
+
+public class Constants {
+	public static final String MOCK_PERSON_NAME = "Harry";
+	public static final Person MOCK_PERSON = new Person();
+	public static final ArrayList<Person> MOCK_PERSON_LIST = new ArrayList<Person>();
+}
