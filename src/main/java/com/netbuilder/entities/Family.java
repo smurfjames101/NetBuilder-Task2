4ffc2656baa1653969bb563c0eec85629909611e
smurfjames101 <smurfjames@gmail.com>
2019-03-30 22:37:10 +0000

    Added Set And Find Options To Family

--- src/main/java/com/netbuilder/entities/Family.java
+++ src/main/java/com/netbuilder/entities/Family.java
@@ -4,4 +4,19 @@ import java.util.ArrayList;
 
 public class Family {
     ArrayList<Person> peopleList = new ArrayList<>();
+    public Person findPerson(String name) {
+        for (Person person:peopleList) {
+            if (person.name.equals(name)) {
+                return person;
+            }
+        }
+        return null;
+    }
+    public boolean setParent(String childName, String parentName) {
+        Person child = findPerson(childName);
+        Person parent = findPerson(parentName);
+        parent.offspring.add(child);
+        child.parents.add(parent);
+        return true;
+    }
 }
