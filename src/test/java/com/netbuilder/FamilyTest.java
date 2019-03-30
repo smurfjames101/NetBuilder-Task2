61206942d1f671ce4595c4c25c8e5f01fe54fd6a
smurfjames101 <smurfjames@gmail.com>
2019-03-30 22:38:52 +0000

    Added Find Person Test

--- src/test/java/com/netbuilder/FamilyTest.java
+++ src/test/java/com/netbuilder/FamilyTest.java
@@ -1,5 +1,19 @@
 package com.netbuilder;
 
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.netbuilder.entities.Family;
+import com.netbuilder.main.Constants;
+
 public class FamilyTest {
 
+	Family family;
+	Constants constants;
+	@Test
+	public void findPerson() {
+		//PersonNotFound
+		assertNull(family.findPerson(constants.MOCK_PERSON_NAME));
+	}
 }
