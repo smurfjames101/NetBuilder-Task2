6f0e210ec5c5b5ae665a8360b81a369ff31b000a
smurfjames101 <smurfjames@gmail.com>
2019-03-31 04:51:19 +0100

    Moved All Assertions Into One Test Due To Inherent Dependency

--- src/test/java/com/netbuilder/FamilyTest.java
+++ src/test/java/com/netbuilder/FamilyTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -13,49 +14,28 @@ import com.netbuilder.main.Constants;
 
 public class FamilyTest {
 
-	public static Family family;
+	public Family family;
 	public Constants constants;
 	public Person mockPerson;
-	@BeforeClass
-	public static void setup(){
+	@Before
+	public void setup(){
 		family = new Family();
-		family.setParent("Frank", "Morgan");
-		family.setParent("Frank", "Dylan");
-        family.male("Dylan"); 
-        family.setParent("Joy", "Frank"); 
-        family.male("Frank");
-        family.setParent("July", "Morgan"); 
-        family.getChildren("Morgan"); 
-        family.setParent("Jennifer", "Morgan"); 
-        family.getChildren("Morgan"); 
-        family.getChildren("Dylan"); 
-        family.getParents("Frank");
-        family.setParent("Frank", "Frank"); 
 	}
 
 	@Test
-	public void setParentTest() {
+	public void familyTest() {
 		assertTrue(family.setParent("Frank", "Morgan"));
 		assertTrue(family.setParent("Frank", "Dylan"));
-		assertTrue(family.setParent("Joy", "Frank")); 
-		assertTrue(family.setParent("July", "Morgan"));
-		assertFalse(family.setParent("Frank", "Frank"));
-		assertTrue(family.setParent("Jennifer", "Morgan"));
-	}
-	@Test
-	public void maleTest() {
 		assertTrue(family.male("Dylan"));
+		assertTrue(family.setParent("Joy", "Frank")); 
 		assertTrue(family.male("Frank"));
         assertFalse(family.male("Morgan")); 
-	}
-	@Test
-	public void getChildrenTest() {
+		assertTrue(family.setParent("July", "Morgan"));
 		assertEquals("[Frank, July]",family.getChildren("Morgan"));
-		assertEquals("[Frank, Jennifer, July]]",family.getChildren("Morgan")); 
+		assertTrue(family.setParent("Jennifer", "Morgan"));		
+		assertEquals("[Frank, July, Jennifer]",family.getChildren("Morgan")); 
         assertEquals("[Frank]",family.getChildren("Dylan"));
-	}
-	@Test
-	public void getParentsTest() {
-		assertEquals("[Dylan,Morgan]",family.getParents("Frank"));
+        assertEquals("[Dylan, Morgan]",family.getParents("Frank"));
+		assertFalse(family.setParent("Frank", "Frank"));
 	}
 }
