e7c8aa9da58ee32a2143887707fab24f1e5f5c3f
smurfjames101 <smurfjames@gmail.com>
2019-03-31 00:40:06 +0000

    Removed Static Poison

--- src/main/java/com/netbuilder/main/Constants.java
+++ src/main/java/com/netbuilder/main/Constants.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import com.netbuilder.entities.Person;
 
 public class Constants {
-	public static final String MOCK_PERSON_NAME = "Harry";
-	public static final String MOCK_PARENT_NAME = "Vera";
-	public static final String MOCK_CHILD_NAME = "Peter";
-	public static final Person MOCK_PERSON = new Person();
-	public static final ArrayList<Person> MOCK_PERSON_LIST = new ArrayList<Person>();
+	public final String MOCK_PERSON_NAME = "Harry";
+	public final String MOCK_PARENT_NAME = "Vera";
+	public final String MOCK_CHILD_NAME = "Peter";
+	public final Person MOCK_PERSON = new Person();
+	public final ArrayList<Person> MOCK_PERSON_LIST = new ArrayList<Person>();
 }
