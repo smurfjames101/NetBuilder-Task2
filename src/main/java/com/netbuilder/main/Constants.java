e44d651d0ce964cdb1957af5ba7639e790b5e894
smurfjames101 <smurfjames@gmail.com>
2019-03-31 03:23:22 +0100

    Update Constants.java

--- src/main/java/com/netbuilder/main/Constants.java
+++ src/main/java/com/netbuilder/main/Constants.java
@@ -6,7 +6,8 @@ import com.netbuilder.entities.Person;
 
 public class Constants {
 	public final String MOCK_PERSON_NAME = "Harry";
-	public final String MOCK_PARENT_NAME = "Vera";
+	public final String MOCK_FATHER_NAME = "Paul";
+	public final String MOCK_MOTHER_NAME = "Vera";
 	public final String MOCK_CHILD_NAME = "Peter";
 	public final Person MOCK_PERSON = new Person();
 	public final ArrayList<Person> MOCK_PERSON_LIST = new ArrayList<Person>();
