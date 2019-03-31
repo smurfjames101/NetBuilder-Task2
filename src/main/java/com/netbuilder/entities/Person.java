ad62981b8ae9c50a9ecac349c53c6db5608edb13
smurfjames101 <smurfjames@gmail.com>
2019-03-31 03:24:36 +0100

    Changed Person To Comply With Polymorphism Principle

--- src/main/java/com/netbuilder/entities/Person.java
+++ src/main/java/com/netbuilder/entities/Person.java
@@ -3,10 +3,41 @@ package com.netbuilder.entities;
 import java.util.ArrayList;
 
 public class Person {
-	String name;
-	Person mother;
-	Person father;
-	String gender;
-	ArrayList<Person> parents = new ArrayList<>(2);
-	ArrayList<Person> offspring = new ArrayList<>();
+	private String name;
+	private Person mother;
+	private Person father;
+	private String gender;
+	 ArrayList<Person> parents = new ArrayList<>(2);
+	 ArrayList<Person> offspring = new ArrayList<>();
+	 
+	 public Person() {
+	}
+	public Person(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Person getMother() {
+		return mother;
+	}
+	public void setMother(Person mother) {
+		this.mother = mother;
+	}
+	public Person getFather() {
+		return father;
+	}
+	public void setFather(Person father) {
+		this.father = father;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	 
 }
