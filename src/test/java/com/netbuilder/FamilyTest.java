f53f4a81446e975c69c66b41b1816cc79790b64d
smurfjames101 <smurfjames@gmail.com>
2019-03-31 03:25:26 +0100

    Added Assessment Example Output As Tests

--- src/test/java/com/netbuilder/FamilyTest.java
+++ src/test/java/com/netbuilder/FamilyTest.java
@@ -1,24 +1,61 @@
 package com.netbuilder;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.netbuilder.entities.Family;
+import com.netbuilder.entities.Person;
 import com.netbuilder.main.Constants;
 
 public class FamilyTest {
 
-	Family family;
-	Constants constants;
-	@Test
-	public void findPersonTest() {
-		//PersonNotFound
-		assertNull(family.findPerson(constants.MOCK_PERSON_NAME));
+	public static Family family;
+	public Constants constants;
+	public Person mockPerson;
+	@BeforeClass
+	public static void setup(){
+		family = new Family();
+		family.setParent("Frank", "Morgan");
+		family.setParent("Frank", "Dylan");
+        family.male("Dylan"); 
+        family.setParent("Joy", "Frank"); 
+        family.male("Frank");
+        family.setParent("July", "Morgan"); 
+        family.getChildren("Morgan"); 
+        family.setParent("Jennifer", "Morgan"); 
+        family.getChildren("Morgan"); 
+        family.getChildren("Dylan"); 
+        family.getParents("Frank");
+        family.setParent("Frank", "Frank"); 
 	}
+
 	@Test
 	public void setParentTest() {
-		assertTrue(family.setParent(constants.MOCK_CHILD_NAME, constants.MOCK_PARENT_NAME));
+		assertTrue(family.setParent("Frank", "Morgan"));
+		assertTrue(family.setParent("Frank", "Dylan"));
+		assertTrue(family.setParent("Joy", "Frank")); 
+		assertTrue(family.setParent("July", "Morgan"));
+		assertFalse(family.setParent("Frank", "Frank"));
+		assertTrue(family.setParent("Jennifer", "Morgan"));
+	}
+	@Test
+	public void maleTest() {
+		assertTrue(family.male("Dylan"));
+		assertTrue(family.male("Frank"));
+        assertFalse(family.male("Morgan")); 
+	}
+	@Test
+	public void getChildrenTest() {
+		assertEquals("[Frank, July]",family.getChildren("Morgan"));
+		assertEquals("[Frank, Jennifer, July]]",family.getChildren("Morgan")); 
+        assertEquals("[Frank]",family.getChildren("Dylan"));
+	}
+	@Test
+	public void getParentsTest() {
+		assertEquals("[Dylan,Morgan]",family.getParents("Frank"));
 	}
 }
